eb7069c9ef8ed9bd8758a2ec4f394cd011f446fb
Eugeny <88819174+EStrukov@users.noreply.github.com>
2022-05-27 16:32:31 +0400

    fix: add one classname style according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -32,11 +32,14 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="list-item";
+
     label.innerText=taskString;
     label.className="label-task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox";
     editInput.type="text";
     editInput.className="input-task";
 
@@ -45,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.className="del-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
